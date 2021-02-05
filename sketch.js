6193fc65910d233e73126a6355e2bf507e57402c
Rishu9835 <76434246+Rishu9835@users.noreply.github.com>
2021-02-05 15:53:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango
 var world,boy;
 
 function preload(){
-	boy=loadImage("images/boy.png");
+	boy=loadImage("boy.png");
   }
 
 function setup() {
